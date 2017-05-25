5dc3f9288553d001672523ba8bffa79778b8d01f
Julian <julianbrendl@gmail.com>
2017-05-25 13:02:01 +0200

    removed logger

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import logger from 'util/logger'
-
 const addArray = function addArray(arr) {
   const result = arr.reduce(function(a, b) {
     return a + b
   }, 0)
-
-  logger.debug('test')
 
   return result
 }
