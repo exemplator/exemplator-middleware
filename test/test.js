8c9319c0424925f22b287a60e911e51de5d74fac
Julian <julianbrendl@gmail.com>
2017-05-25 15:48:35 +0200

    added first debugging test case

--- test/test.js
+++ test/test.js
@@ -1,15 +1,18 @@
 import {expect} from 'chai'
 import {it, describe} from 'mocha'
+import J8Parser from '../src/antlr/java8/Java8Parser'
+import JExamples from './resources/java/example1'
 
 describe('Perform sample test\n', () => {
-    sampleTest()
+  antlrDebuging()
 })
 
 /**
- * Sample test
+ * Debugging for antlr
  */
-function sampleTest() {
-  it('Test case for testing purposes: ', () => {
-    expect('hello world').to.equal('hello world')
+function antlrDebuging() {
+  it('Debugging antlr: ', () => {
+    J8Parser.Java8Parser(JExamples.code)
+    expect(1).to.equal(1)
   })
 }
