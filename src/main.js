69f58e960e6b2e70bcc5e7124834a5e102b85ccf
Julian <julianbrendl@gmail.com>
2017-05-25 13:05:40 +0200

    added template for main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-const addArray = function addArray(arr) {
-  const result = arr.reduce(function(a, b) {
-    return a + b
-  }, 0)
-
-  return result
+const Middleware = {
 }
 
-module.exports = addArray
+export default Middleware
