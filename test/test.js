ebca528cb4adc72ef3e06ebf12656fa840b6b2bb
Julian <julianbrendl@gmail.com>
2017-05-26 15:21:26 +0200

    working on visitor

--- test/test.js
+++ test/test.js
@@ -11,31 +11,39 @@ describe('Perform sample test\n', () => {
  */
 function antlrDebuging() {
   it('Debugging antlr: ', () => {
-    const antlr4 = require('antlr4/index');
-    const Java8Lexer = require('../src/antlr/java8/Java8Lexer');
-    const Java8Parser = require('../src/antlr/java8/Java8Parser');
-    const Java8Listener = require('../src/antlr/java8/Java8Listener');
+    const antlr4 = require('antlr4/index')
+    const Java8Lexer = require('../src/antlr/java8/Java8Lexer')
+    const Java8Parser = require('../src/antlr/java8/Java8Parser')
+    const Java8Visitor = require('../src/antlr/java8/Java8Visitor')
 
-    const J8Parser = function() {
-      Java8Listener.Java8Listener.call(this)
-      return this;
+    /**
+     * Sample Visitor that doesn't work
+     * @return {Visitor}
+     * @constructor
+     */
+    function Visitor() {
+      Java8Visitor.Java8Visitor.call(this)
+      return this
     }
-    J8Parser.prototype = Object.create(Java8Listener.Java8Listener.prototype)
-    J8Parser.prototype.constructor = J8Parser
+
+    Visitor.prototype = Object.create(Java8Visitor.Java8Visitor.prototype)
+    Visitor.prototype.constructor = Visitor
+    Visitor.prototype.visitTest = function(ctx) {
+      // implement logic to determine which function to visit
+      // then call next function and with the right context
+      this.visitPackageDeclaration(ctx)
+    }
 
     const input = JExamples.code
     const chars = new antlr4.InputStream(input)
     const lexer = new Java8Lexer.Java8Lexer(chars)
     const tokens = new antlr4.CommonTokenStream(lexer)
     const parser = new Java8Parser.Java8Parser(tokens)
+    const visitor = new Visitor()
     parser.buildParseTrees = true
-    const tree = parser.compilationUnit()
+    const tree = parser.packageDeclaration()
 
-    // works up until here
-
-    // const tree = parser.date();
-    // const printer = new YearPrinter();
-    // antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree)
+    visitor.visitTest(tree)
 
     expect(1).to.equal(1)
   })
